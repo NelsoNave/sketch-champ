b78de679270e6584d83e80a3bdcabc6f78427fba
Risa Yamamoto <yamari2422@gmail.com>
2024-12-04 15:04:45 -0800

    feat: the pending room component #2

---
+++ frontend/src/components/PendingRoom.tsx
@@ -0,0 +1,31 @@
+import DrawingCanvas from "./DrawingCanvas";
+import Button from "./Button";
+
+type Props = {};
+
+const Room = (props: Props) => {
+  const handleGetReady = () => {
+    // let the server know that user is ready.
+  };
+
+  return (
+    <div className="flex gap-8">
+      <div className="hidden sm:block w-2/3">
+        <DrawingCanvas />
+      </div>
+      <div className="flex flex-col h-full w-full sm:w-1/3 gap-4">
+        <div className="h-[200px] w-full sm:w-full sm:h-auto sm:flex-grow rounded-xl border-2 border-black bg-white">
+          Room settings
+        </div>
+        <div className="h-[200px] w-full sm:w-full sm:h-auto sm:flex-grow rounded-xl border-2 border-black bg-white">
+          player
+        </div>
+        <Button variant="pink" onClick={handleGetReady}>
+          I'm ready to join!
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Room;
