cc9aa06e9475bc0a15bc0309606d2defe55fd696
Risa Yamamoto <yamari2422@gmail.com>
2024-12-04 01:12:42 -0800

    feat: login / sign up validation by using react-hook-form #1

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "./Button";
+
+type FormData = {
+  username: string;
+  password: string;
+};
 
 const LoginModal = ({ closeModal }: { closeModal: () => void }) => {
   const [isSignup, setIsSignup] = useState(false);
   const setSignup = () => setIsSignup(true);
+
+  // validation
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = () => {
+    closeModal();
+  };
 
   return (
     <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center ">
@@ -19,16 +36,28 @@ const LoginModal = ({ closeModal }: { closeModal: () => void }) => {
             <img src="../../public/close.svg" alt="close button" />
           </button>
         </div>
-        <form className="w-full">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2 rounded-lg">
             <label className="text-xs font-semibold font-poppins">
               User Name
             </label>
             <input
               type="text"
-              className="w-full px-4 py-2 border-1.5 border-black rounded-lg text-sm h-[50px] font-poppins"
+              {...register("username", {
+                required: "Username is required",
+                maxLength: {
+                  value: 8,
+                  message: "Username must be up to 8 characters",
+                },
+              })}
+              className="w-full px-4 py-2 border-1.5 border-black rounded-lg text-sm h-[50px]"
               placeholder="Enter username"
             />
+            {errors.username && (
+              <span className="text-red-500 text-xs">
+                {errors.username.message}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <label className="text-xs font-semibold font-poppins">
@@ -36,9 +65,15 @@ const LoginModal = ({ closeModal }: { closeModal: () => void }) => {
             </label>
             <input
               type="password"
+              {...register("password", { required: "Password is required" })}
               className="w-full px-4 py-2 border-1.5 border-black rounded-lg text-sm h-[50px] font-poppins"
               placeholder="Enter your password"
             />
+            {errors.password && (
+              <span className="text-red-500 text-xs">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <Button className="w-full mt-4" variant="pink">
             {isSignup ? "Sign up" : "Log In"}
