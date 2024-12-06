11ead7eb66019a154117fc116c5746dcda568bef
Risa Yamamoto <yamari2422@gmail.com>
2024-12-05 18:54:23 -0800

    fix:  the UI after logging out #14

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,15 +6,26 @@ const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
-  const { isLoggedIn } = useAuthStore();
+  const { isLoggedIn, logout } = useAuthStore();
+
+  // logout
+  const onLogoutClick = () => {
+    logout();
+  };
 
   return (
     <div className="flex justify-between items-center py-5 px-2 md:px-8">
       <div className="font-russo_one">Kawaii Logo</div>
       <nav>
-        <button onClick={openModal} className="text-md font-bold">
-          {isLoggedIn ? "Log out" : "Log in"}
-        </button>
+        {isLoggedIn ? (
+          <button onClick={onLogoutClick} className="text-md font-bold">
+            Log out
+          </button>
+        ) : (
+          <button onClick={openModal} className="text-md font-bold">
+            Log in
+          </button>
+        )}
       </nav>
       {isModalOpen && <LoginModal closeModal={closeModal} />}
     </div>
