62737ef8edb2bf3f48853aa52ff5cf982762ac42
Risa Yamamoto <yamari2422@gmail.com>
2024-12-08 11:33:30 -0800

    feat: navigate to result page once a game finished #25

--- frontend/src/store/useResultStore.ts
+++ frontend/src/store/useResultStore.ts
@@ -3,12 +3,14 @@ import { Result } from "../types/result.type";
 
 interface ResultStore {
   results: Result[];
-  setResult: (result: Result) => void;
+  setResult: (result: Result[]) => void;
 }
 
 export const useResultStore = create<ResultStore>((set) => ({
   results: [],
-  setResult: (result: Result) => {
-    set((state) => ({ results: [...state.results, result] }));
+  setResult: (result) => {
+    set(() => ({
+      results: result,
+    }));
   },
 }));
